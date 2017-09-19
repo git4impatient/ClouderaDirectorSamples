3049d7a01c7cfca23f727274866165f0d5f9c4de
git4impatient <lurie@lurie.biz>
2017-09-19 17:34:40 -0400

    Create creds.sh

---
+++ creds.sh
@@ -0,0 +1,6 @@
+export AWS_ACCESS_KEY_ID="AAAAAAAAAAAAAAAAAAMA"
+export AWS_SECRET_ACCESS_KEY='99999999999999999999999999999999999999Ir'
+export subnetId='subnet-7777777e'
+export securityGroupsIds='sg-2222222b'
+export path2privateKey='/home/ec2-user/directorV2.pem'
+bash
