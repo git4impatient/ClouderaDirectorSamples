3557b5162c37d32c075920233b1da46253f306c1
git4impatient <lurie@lurie.biz>
2017-10-05 17:02:41 -0400

    Update creds.sh

--- creds.sh
+++ creds.sh
@@ -2,5 +2,8 @@ export AWS_ACCESS_KEY_ID="AAAAAAAAAAAAAAAAAAMA"
 export AWS_SECRET_ACCESS_KEY='99999999999999999999999999999999999999Ir'
 export subnetId='subnet-7777777e'
 export securityGroupsIds='sg-2222222b'
-export path2privateKey='/home/ec2-user/directorV2.pem'
+# you need to upload your private key .pem file and then specificy the path on the director
+# node as to where it lives
+export path2privateKey='/home/centos/.ssh/myprivatekey.pem'
+# start a new shell so people do not have to remember to source the file
 bash
