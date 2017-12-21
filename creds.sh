eb5d2ece666cc95aa592b98c5a5f871b9af9a677
git4impatient <lurie@lurie.biz>
2017-12-21 08:30:56 -0500

    Update creds.sh

--- creds.sh
+++ creds.sh
@@ -1,9 +1,14 @@
-export AWS_ACCESS_KEY_ID="AAAAAAAAAAAAAAAAAAMA"
-export AWS_SECRET_ACCESS_KEY='99999999999999999999999999999999999999Ir'
-export subnetId='subnet-7777777e'
-export securityGroupsIds='sg-2222222b'
+#accessKeyId: ${?AWS_ACCESS_KEY_ID}
+#secretAccessKey: ${?AWS_SECRET_ACCESS_KEY}
+
+export AWS_ACCESS_KEY_ID="xxxxxxxxxxxxxxxxx2MA"
+export AWS_SECRET_ACCESS_KEY='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxIr'
+export subnetId='subnet-740e482eREPLACEWITHYOURS'
+export securityGroupsIds='sg-2b2dab5bREPLACEWITHYOURS'
 # you need to upload your private key .pem file and then specificy the path on the director
 # node as to where it lives
-export path2privateKey='/home/centos/myprivatekey.pem'
-# start a new shell so people do not have to remember to source the file
+export path2privateKey='/home/ec2-user/REPLACEWITHYOURSdirectorV2.pem'
+export region='us-east-1'
+# This centos7 image has worked well for several examples
+export image='ami-6d1c2007'
 bash
